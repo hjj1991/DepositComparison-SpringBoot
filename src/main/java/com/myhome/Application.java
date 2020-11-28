f42dc39b49d577165dc7e8d6068d546c7c648368
JaeJeong Hwang <hjj19911@naver.com>
2020-11-29 01:43:21 +0900

    이동..

--- src/main/java/com/myhome/Application.java
+++ src/main/java/com/myhome/Application.java
@@ -1,47 +1,47 @@
-//package com.myhome;
-//
-//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.SpringBootConfiguration;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
-//import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.web.client.RestTemplate;
-//import org.springframework.web.filter.HiddenHttpMethodFilter;
-//
-//@EnableJpaAuditing
-//@EnableBatchProcessing //배치기능 활성화
-//@EnableScheduling
-//@SpringBootApplication
-//public class Application {
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//
-//    /**
-//     * HiddenHttpMethodFilter
-//     */
-//    @Bean
-//    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
-//        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
-//        return filter;
-//    }
-//
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public RestTemplate getRestTemplate() {
-//        return new RestTemplate();
-//    }
-//}
+package com.myhome;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@EnableJpaAuditing
+@EnableBatchProcessing //배치기능 활성화
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * HiddenHttpMethodFilter
+     */
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+        return filter;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
